1dbb4d887fd97801b2a5bef76d2acecf1ed1cb07
DarlingKeyuM <32640786+DarlingKeyuM@users.noreply.github.com>
2017-10-11 17:45:36 +0800

    vv

---
+++ website1/index.php
@@ -0,0 +1,12 @@
+<?php include ("./inc/header.php")?>
+<?php include ("./inc/header.php")?>
+
+	<h1>Home</h1>
+<?php include "./inc/footer.php"?>
+/*
+include&require区别
+如果有报错信息.include会显示,但不影响后面代码的执行
+如果有报错信息.require会显示,后面代码不会执行
+include_once&require_once区别
+重复引入,只识别一次
+*/
